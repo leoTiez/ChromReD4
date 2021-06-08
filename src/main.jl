452c133e29ad16c7cd51f8f032311bc63d478c5e
Leo Zeitler <leozeitler95@gmail.com>
2021-06-08 09:21:40 +0200

    Update: Creation of gif

--- src/main.jl
+++ src/main.jl
@@ -12,23 +12,39 @@ function main()
     asso_rad = .3
     disso_rad = .05
 
+    save_gif = false
+    show_plots = true
+    frames = []
     nuc = init_nucleus(len_chromatin, size_nucleus)
     for _ in 1:upd_steps
         update!(nuc, asso_prob=asso_no, disso_prob=disso_no, diffu=diffu_const)
         graph = plot(nuc, "Before Radiation")
-        plt.display(graph)
-        plt.gui()
-        sleep(0.5)
+        push!(frames, graph)
+        if show_plots
+            plt.display(graph)
+            plt.gui()
+        end
+        sleep(0.1)
     end
 
     for _ in 1:upd_steps
         update!(nuc, asso_prob=asso_rad, disso_prob=disso_rad, diffu=diffu_const)
         graph = plot(nuc, "After Radiation")
-        plt.display(graph)
-        plt.gui()
-        sleep(0.5)
+        push!(frames, graph)
+        if show_plots
+            plt.display(graph)
+            plt.gui()
+        end
+        sleep(0.1)
     end
-    return nuc
+
+    if save_gif
+        animation_gif = plt.Animation()
+        for i in size(frames)
+            plt.frame(animation_gif, frames[i])
+        end
+        plt.gif(animation_gif, "ChromReD4_example.gif", fps = 30)
+    end
 end
 
 main()
